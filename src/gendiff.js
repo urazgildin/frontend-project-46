f3e44cee939fb69d88c756a14281bbae0e4b1192
anton urazgildin <antoha91@mail.ru>
2023-08-16 20:01:27 +0300

    4 step refactoring v4

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,25 +5,21 @@ const getDifferencies = (filepath1, filepath2) => {
   const obj1 = getParsedData(getAbsolutePath(filepath1));
   const obj2 = getParsedData(getAbsolutePath(filepath2));
 
-  const keys = [...Object.keys(obj1), ...Object.keys(obj2)];
-  const sortedKeys = _.sortBy(keys);
-  const uniqKeys = _.uniq(sortedKeys);
-  const dublicats = sortedKeys.filter((key, index) => sortedKeys.indexOf(key) !== index);
+  const uniqKeys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedKeys = _.sortBy(uniqKeys);
 
-  const resultAsArr = uniqKeys.reduce((acc, key) => {
-    if (dublicats.includes(key)) {
-      if (obj1[key] === obj2[key]) {
-        acc.push(`  ${key}: ${obj1[key]}`);
-      } else {
-        acc.push(`- ${key}: ${obj1[key]}`, `+ ${key}: ${obj2[key]}`);
-      }
+  const resultAsArr = sortedKeys.reduce((acc, key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      acc.push(`+ ${key}: ${obj2[key]}`);
     }
-
-    if (!dublicats.includes(key)) {
-      if (Object.hasOwn(obj1, key)) {
-        acc.push(`- ${key}: ${obj1[key]}`);
+    if (!Object.hasOwn(obj2, key)) {
+      acc.push(`- ${key}: ${obj1[key]}`);
+    }
+    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      if (!_.isEqual(obj1[key], obj2[key])) {
+        acc.push(`- ${key}: ${obj1[key]}`, `+ ${key}: ${obj2[key]}`);
       } else {
-        acc.push(`+ ${key}: ${obj2[key]}`);
+        acc.push(`  ${key}: ${obj1[key]}`);
       }
     }
     return acc;
