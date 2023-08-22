f9e125cb9fa8962f31e1d3749584487ef44cd670
anton urazgildin <antoha91@mail.ru>
2023-08-22 09:39:44 +0300

    4 step refactoring v5

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -3,5 +3,5 @@ import { readFixtureFile } from '../src/utils.js';
 import getDifferencies from '../src/gendiff.js';
 
 test('get differencies of two files', () => {
-  expect(getDifferencies('__fixtures__/file1.json', '__fixtures__/file1.json')).toEqual(readFixtureFile('expectedFile.json'));
+  expect(getDifferencies('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(readFixtureFile('expectedFile.txt'));
 });
